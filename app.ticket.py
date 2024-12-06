605bd6756cc32b5868215ac237f44f7402bd51c7
FacuDex96 <118861304+FacuDex96@users.noreply.github.com>
2024-12-06 15:14:40 -0300

    Add files via upload

---
+++ app.ticket.py
@@ -0,0 +1,41 @@
+print("Hola, bienvenido al sistema de tickets")
+
+import pickle, sys, os, random
+
+os.system("cls")
+numero_random= random.randrange(1000,9999) #se crea un numero random
+
+lista_ticket = ["Generar un nuevo ticket", "Leer un ticket", "Salir"]
+
+lista_ticket.append("Generar un nuevo ticket")
+lista_ticket.append("Leer un ticket")
+lista_ticket.append("Salir")
+
+
+diccionario = {
+    "Su nombre: Facundo",
+    "Sector: Tecno3F"
+    "Nº de Ticket: 4387"
+    "Asunto: Erro en la Matrix"
+    "Mensaje: Ni se te ocurra prender la PC"}
+
+
+
+
+"""
+with open(guardar, "wb")as f:
+    pickle.dump(ticket,f)
+    
+with open(abrir, "wb") as f:
+    pickle.dumps(ticket,f)
+     """
+     
+     
+     
+     
+     
+     
+     
+     
+     
+sys.exit()   
